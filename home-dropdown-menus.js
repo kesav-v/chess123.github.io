f8396e22e1c781b9c2a6457b54e82fe41e808f9f
chess123 <kesav@viswanadha.net>
2016-02-07 09:44:52 -0800

    changes to homepage

---
+++ home-dropdown-menus.js
@@ -0,0 +1,20 @@
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction(n) {
+    document.getElementById("myDropdown" + n).classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn') && !event.target.matches('.drpdntext')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+};
