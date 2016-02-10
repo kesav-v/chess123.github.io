796fbcc2c0c922ffebe1747d8bfdf1f6beb9eb11
chess123 <kesav@viswanadha.net>
2016-02-09 21:50:56 -0800

    added to homepage

--- reveal-animation.js
+++ reveal-animation.js
@@ -1,5 +1,6 @@
 setTimeout(reveal_msg, 1000);
 setTimeout(reveal_button, 2000);
+setTimeout(reveal_facets, 3000);
 
 function reveal_msg() {
 	$('#intromsg').animate({opacity: '1'}, 1000);
@@ -7,4 +8,8 @@ function reveal_msg() {
 
 function reveal_button() {
 	$('#introbutton').animate({opacity: '1'}, 1000);
+}
+
+function reveal_facets() {
+	$('#facets').animate({opacity: '1'}, 1000);
 }
