67a40685fb48f853490c9929503ecb44c2196560
chess123 <kesav@viswanadha.net>
2016-02-10 14:39:11 -0800

    changed favicon, deleted cname

--- reveal-animation.js
+++ reveal-animation.js
@@ -1,6 +1,16 @@
-setTimeout(reveal_msg, 1000);
-setTimeout(reveal_button, 2000);
-setTimeout(reveal_facets, 3000);
+var thecover;
+var the_bottom;
+window.onload = function() {
+	thecover = document.getElementById("facet-cover");
+	thecover.style.height = document.getElementById("facet-container").offsetHeight;
+	thecover.style.width = document.getElementById("facet-container").offsetWidth;
+	thecover.style.bottom = document.getElementById("facet-container").offsetBottom;
+	thecover.style.left = document.getElementById("facet-container").offsetLeft;
+	the_bottom = thecover.offsetBottom;
+	setTimeout(reveal_msg, 1000);
+	setTimeout(reveal_button, 2000);
+	setTimeout(reveal_facets, 3000);
+};
 
 function reveal_msg() {
 	$('#intromsg').animate({opacity: '1'}, 1000);
@@ -11,5 +21,7 @@ function reveal_button() {
 }
 
 function reveal_facets() {
-	$('#facets').animate({opacity: '1'}, 1000);
+	document.getElementById("facet-container").style.visibility = "visible";
+	$('#facet-cover').animate({top: the_bottom + 'px'}, 1000);
+	console.log(thecover.offsetBottom);
 }
