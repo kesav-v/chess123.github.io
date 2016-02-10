8974f4689ded0a70c0c106a9001948333e2a2974
chess123 <kesav@viswanadha.net>
2016-02-09 16:06:01 -0800

    smoother homepage

--- home-dropdown-menus.js
+++ home-dropdown-menus.js
@@ -1,24 +1,39 @@
+var current_open;
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
 function myFunction(n) {
+  if (document.getElementById("myDropdown" + n).classList.contains("show")) {
+    console.log("closing");
+    var theheight = document.getElementById("myDropdown" + n).offsetHeight;
+    $('#myDropdown' + n).animate({height: "0px"}, 500, function() {
+      document.getElementById("myDropdown" + n).classList.toggle("show");
+      document.getElementById("myDropdown" + n).style.height = theheight + "px";
+    });
+    return;
+  }
   for (i = 0; i < 5; i++) {
     if (i == n - 1) continue;
     document.getElementById("myDropdown" + (i + 1)).classList.remove("show");
   }
+  current_open = n;
   document.getElementById("myDropdown" + n).classList.toggle("show");
+  var theheight = document.getElementById("myDropdown" + n).offsetHeight;
+  console.log(theheight);
+  document.getElementById("myDropdown" + n).style.height = "0px";
+  $('#myDropdown' + n).animate({height: theheight + "px"}, 500);
+}
+
+function numAs(elem) {
+  var cnt = 0;
+  for (i = 0; i < elem.length - 1; i++) {
+    if (elem.substring(i, i + 2) === "<a") cnt++;
+  }
+  return cnt;
 }
 
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
   if (!event.target.matches('.dropbtn') && !event.target.matches('.drpdntext')) {
-
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
+    if (document.getElementById("myDropdown" + current_open).classList.contains("show")) myFunction(current_open);
   }
 };
