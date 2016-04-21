eae80051f17edc788f5cc4fee2520793eca370c6
chess123 <kesav@viswanadha.net>
2016-04-20 23:06:43 -0700

    another improved encryption

--- encrypt.js
+++ encrypt.js
@@ -3,13 +3,13 @@ window.onload = function() {
 	console.log("loaded");
 }
 
-const x = "68%;%#-8247$471$5-5$-1$-47-4763%;%#-7747$471$5-5$-1$-47122%;%#-9047$471$5-5$-1$124%;%#-9647$471$5-5$129%;%#-9147$471$5123%;%#-8947$471$110%;%#-3047$47115%;%#-3547$";
+const y = "-6241447$471$5-5$-1$-47-47-5654547$471$5-5$-1$-47-7271147$471$5-5$-1$-8085047$471$5-5$-7398247$471$5-7134947$471$-1495147$47-1814947$";
 
 function encrypt(s) {
 	var str = "";
 	for (i = 0; i < s.length; i++) {
-		str += String(s.charCodeAt(s.length - i - 1) + 13) + "%;%#";
-		str += String(20 - s.charCodeAt(i));
+		x = s.charCodeAt(i);
+		str += String(Math.round(Math.cos(x) * (1 + x) - 6 * x * x));
 		for (j = 0; j < s.length - i; j++) {
 			str += String(s.charCodeAt(j) - s.charCodeAt(s.length - j - 1));
 			if (s.charCodeAt(j) % s.charCodeAt(0) <= 10) str += "$";
@@ -21,6 +21,6 @@ function encrypt(s) {
 function find_string(e) {
 	if (e.which != 13) return;
 	var str = encrypt($('input[name="input1"]').val());
-	if (str === x) document.getElementById("text1").innerHTML = "Login succeeded.";
+	if (str === y) document.getElementById("text1").innerHTML = "Login succeeded.";
 	else document.getElementById("text1").innerHTML = "Login failed.";
 }
