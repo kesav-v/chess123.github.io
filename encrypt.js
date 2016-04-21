9f72c74bbe37d47488866a998408dc7e8543ea71
chess123 <kesav@viswanadha.net>
2016-04-20 22:38:45 -0700

    improved password encryption

--- encrypt.js
+++ encrypt.js
@@ -2,13 +2,18 @@ window.onload = function() {
 	$('input[name="input1"]').keypress(find_string);
 	console.log("loaded");
 }
-const x = "128%;%#-94113%;%#-77124%;%#-78124%;%#-78132%;%#-88114%;%#-81121%;%#-99111%;%#-91111%;%#-91110%;%#-80127%;%#-95";
+
+const x = "68%;%#-8247$471$5-5$-1$-47-4763%;%#-7747$471$5-5$-1$-47122%;%#-9047$471$5-5$-1$124%;%#-9647$471$5-5$129%;%#-9147$471$5123%;%#-8947$471$110%;%#-3047$47115%;%#-3547$";
 
 function encrypt(s) {
 	var str = "";
 	for (i = 0; i < s.length; i++) {
 		str += String(s.charCodeAt(s.length - i - 1) + 13) + "%;%#";
 		str += String(20 - s.charCodeAt(i));
+		for (j = 0; j < s.length - i; j++) {
+			str += String(s.charCodeAt(j) - s.charCodeAt(s.length - j - 1));
+			if (s.charCodeAt(j) % s.charCodeAt(0) <= 10) str += "$";
+		}
 	}
 	return str;
 }
