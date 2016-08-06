65472a193a7d816aaa950967c4fffc283d276474
new-javacc <mainstringmonkeys@gmail.com>
2016-08-06 16:04:26 -0700

    update 7

--- ratings.js
+++ ratings.js
@@ -78,5 +78,5 @@ function getXML() {
 
 function readData(xml) {
 	var xmlDoc = xml.responseXML;
-	document.getElementById("xml-test").innerHTML = xmlDoc.getElementsByTagName("name")[1].childNodes.length + "******";
+	document.getElementById("xml-test").innerHTML = xmlDoc.getElementsByTagName("name")[1].childNodes[0] + "******";
 }
